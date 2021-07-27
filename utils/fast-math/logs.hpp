9076034ec594e3830bcecc9d334a6920ce096aad
Daniel Holladay <danl@lanl.gov>
2021-07-27 11:45:57 -0600

    Adding optimized log implementation using frexp and approximation to log on [0.5,1) with the help of Jonah.

--- utils/fast-math/logs.hpp
+++ utils/fast-math/logs.hpp
@@ -31,14 +31,39 @@
 
 namespace BDMath {
 
-  PORTABLE_INLINE_FUNCTION
+  PORTABLE_FORCEINLINE_FUNCTION
   float log10(const float x) {
     // const double ILOG10 = 1./std::log(10.0);
     constexpr double ILOG10 = 0.43429448190325176;
     #if BD_USE_FMATH
     return fmath::log(x)*ILOG10;
     #else
-    return logf(x)*ILOG10;
+    int n{};
+    constexpr const double LOG2OLOG10 = 0.301029995663981195;
+    const float y = frexpf(x, &n);
+    // 7the order approximation to log(x) x in [0.5, 1.0)
+//    const float expr = -2.808428971 + y*(
+//                        8.648808309 + y*(
+//                      -15.910426569 + y*(
+//                       21.53943522  + y*(
+//                      -19.56870772  + y*(
+//                       11.318698784 +   (
+//		       -3.770730277 + 0.5513512194*y)*y)))));
+    // 5th order approximation to log(x) x in [0.5, 1.0)
+//    const float expr = -2.5396743497743 + y*(
+//                        6.420250933346  + y*(
+//                       -8.150589926467  + y*(
+//                        6.830564334687  + (
+//		       -3.192132453902  + 0.6315814621098*y)*y)));
+    // 4th order approximation to log(x) x in [0.5, 1.0)
+    const float expr = -2.36697629372863 + y*(
+			5.2672858163176  + y*(
+                       -5.1242620871906  +   (
+                        2.91607506431871 - 0.69212249971711*y)*y));
+    // log10 x using frexp
+    return ILOG10*expr + n*LOG2OLOG10;
+    // true log 10 x
+//    return logf(x)*ILOG10;
     #endif
   }
 
