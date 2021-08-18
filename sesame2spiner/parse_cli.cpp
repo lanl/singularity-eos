aff150003ac238d35471b21e7a0645403a8607db
Jonah Miller <jonahm@lanl.gov>
2021-08-18 17:07:25 -0600

    removed json and it all seems to work

---
+++ sesame2spiner/parse_cli.cpp
@@ -0,0 +1,93 @@
+//======================================================================
+// sesame2spiner tool for converting eospac to spiner
+// Author: Jonah Miller (jonahm@lanl.gov)
+// © 2021. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S.  Department of Energy/National
+// Nuclear Security Administration. All rights in the program are
+// reserved by Triad National Security, LLC, and the U.S. Department of
+// Energy/National Nuclear Security Administration. The Government is
+// granted for itself and others acting on its behalf a nonexclusive,
+// paid-up, irrevocable worldwide license in this material to reproduce,
+// prepare derivative works, distribute copies to the public, perform
+// publicly and display publicly, and to permit others to do so.
+//======================================================================
+
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+
+#include "parse_cli.hpp"
+#include "io_eospac.hpp"
+
+void parseCLI(int argc, char* argv[],
+	      std::string& savename,
+	      std::vector<std::string>& filenames,
+	      bool& printMetadata,
+	      Verbosity& eospacWarn,
+	      std::string& helpMessage) {
+
+  filenames.clear();
+
+  std::stringstream helpStream;
+  helpStream << "Usage: " << argv[0] << "[-p] [-w] [-h] [-v] [-vv] [-d] <savename> <parameter files>\n\n"
+	     << "\t <savename>: filename to save to\n"
+	     << "\t <parameter files>: input files, one per material\n"
+	     << "\t-p:  print metadata associated with materials "
+	     << "in parameter files\n"
+	     << "\t-v:  print eospac warnings\n"
+	     << "\t-vv: print debug information\n"
+	     << "\t-w:  same as -v\n"
+	     << "\t-d:  same as -vv\n"
+	     << "\t-h:  print this message\n"
+	     << "\n"
+	     << "Several example input files:\n"
+	     << EXAMPLESTRING
+	     << "\n"
+	     << std::endl;
+  helpMessage = helpStream.str();
+
+  bool savename_set = false;
+
+  if (argc < 2) {
+    std::cerr << helpMessage << std::endl;
+    std::exit(1);
+  }
+  if ( argc == 2 && std::strcmp(argv[1],"-h") == 0 ) {
+    std::cout << helpMessage << std::endl;
+    std::exit(0);
+  }
+  for (int i = 1; i < argc; i++) {
+    if ( std::strcmp(argv[i],"-h") == 0 ) {
+      std::cout << helpMessage << std::endl;
+      std::exit(0);
+    }
+    else if ( std::strcmp(argv[i],"-p") == 0 ) {
+      printMetadata = true;
+    } else if ( (std::strcmp(argv[i],"-w") == 0 ||
+                 std::strcmp(argv[i],"-v") == 0 )
+                && eospacWarn == Verbosity::Quiet) {
+      eospacWarn = Verbosity::Verbose;
+    } else if ( (std::strcmp(argv[i],"-d") == 0 ||
+                 std::strcmp(argv[i],"-vv") == 0 )
+                && eospacWarn != Verbosity::Debug) {
+      eospacWarn = Verbosity::Debug;
+    } else {
+      if (!savename_set) {
+	savename = argv[i];
+	savename_set = true;
+      } else {
+	filenames.push_back(std::string(argv[i]));
+      }
+    }
+  }
+  if (filenames.size() < 1) {
+    std::cout << helpMessage << std::endl;
+    std::exit(1);
+  }
+}
---
+++ sesame2spiner/parse_cli.hpp
@@ -0,0 +1,73 @@
+//======================================================================
+// sesame2spiner tool for converting eospac to spiner
+// Author: Jonah Miller (jonahm@lanl.gov)
+// © 2021. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S.  Department of Energy/National
+// Nuclear Security Administration. All rights in the program are
+// reserved by Triad National Security, LLC, and the U.S. Department of
+// Energy/National Nuclear Security Administration. The Government is
+// granted for itself and others acting on its behalf a nonexclusive,
+// paid-up, irrevocable worldwide license in this material to reproduce,
+// prepare derivative works, distribute copies to the public, perform
+// publicly and display publicly, and to permit others to do so.
+//======================================================================
+
+#ifndef _SESAME2SPINER_PARSER_HPP_
+#define _SESAME2SPINER_PARSER_HPP_
+
+#include <string>
+#include <vector>
+#include "io_eospac.hpp"
+
+const std::string EXAMPLESTRING = R"(
+# air.dat
+# These are comments. 
+# The "#" character must be at the beginning of a line.
+# only matid is required. All others override defaults.
+matid = 5030
+name = air
+# rho is in g/cm^3
+rhomin = 1e-2
+rhomax = 10
+numrho = 64
+# T is in Kelvin
+Tmin = 252
+Tmax = 1e4
+numT = 32
+# sie is in erg/g
+siemin = 1e12
+siemax = 1e16
+numsie = 32
+
+
+# titanium.dat
+matid = 2961
+name = titanium
+# These set the number of grid poitns per decade
+# for each variable. The default is 50 points.
+numrho/decade = 30
+numT/decade = 25
+numSie/decade = 15
+
+
+# steel.dat
+matid=4272
+rhomin = 1e-2
+Tmin = 1
+# These shrink lograithm of bounds
+# by a fraction of the total interval <= 1
+shrinklRhoBounds = 0.15
+shrinklTBounds = 0.15
+shrinkleBounds = 0.5
+)";
+
+void parseCLI(int argc, char* argv[],
+	      std::string& savename,
+	      std::vector<std::string>& filenames,
+	      bool& printMetadata,
+	      Verbosity& eospacWarn,
+	      std::string& helpMessage);
+
+#endif // _SESAME2SPINER_PARSER_HPP_
