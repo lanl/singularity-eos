e80fa21ae46a7c4ffff3ce0f382530a17c82c55d
Christopher Mauney <mauneyc@lanl.gov>
2021-12-23 10:39:03 -0700

    changes to dependency tree, first-pass of cmake config gen

--- spack-repo/packages/singularity-eos/package.py
+++ spack-repo/packages/singularity-eos/package.py
@@ -21,13 +21,10 @@ class SingularityEos(CMakePackage, CudaPackage):
     variant("format", default=False, description="Clang-Format Support")
 
     #depends_on("mpark-variant")
-    depends_on("hdf5+cxx+hl~mpi", when="~mpi")
-    depends_on("hdf5+cxx+hl+mpi", when="+mpi")
     depends_on("eospac")
-    depends_on("py-h5py")
 
     depends_on("cmake@3.12:")
-    depends_on("eigen@3.3.9", when="~kokkos-kernels")
+    depends_on("eigen@3.3.9", when="~cuda")
     depends_on("catch2@2.13.4:2.13.6", when="+tests")
 
     depends_on("py-sphinx", when="+doc")
@@ -40,13 +37,21 @@ class SingularityEos(CMakePackage, CudaPackage):
         depends_on("kokkos@3.3:" +_flag, when="+kokkos" + _flag)
         depends_on("kokkos-kernels" + _flag, when="+kokkos-kernels" + _flag)
 
+#   NOTE: we can do depends_on("libfoo cppflags='-fPIC -O2'") for compiler options
     conflicts("+cuda", when="~kokkos")
     conflicts("+openmp", when="~kokkos")
     conflicts("+kokkos-kernels", when="~kokkos")
 
     depends_on("kokkos@3.3:~shared+wrapper+cuda_lambda+cuda_relocatable_device_code", when="+cuda+kokkos")
-    depends_on("kokkos-nvcc-wrapper~mpi", when="+cuda+kokkos~mpi")
-    depends_on("kokkos-nvcc-wrapper+mpi", when="+cuda+kokkos+mpi")
+#    depends_on("kokkos-nvcc-wrapper~mpi", when="+cuda+kokkos~mpi")
+#    depends_on("kokkos-nvcc-wrapper+mpi", when="+cuda+kokkos+mpi")
+
+    for _flag in ("~mpi", "+mpi"):
+        depends_on("hdf5+cxx+hl" + _flag, when=_flag)
+        depends_on("py-h5py" + _flag, when=_flag)
+        depends_on("kokkos-nvcc-wrapper" + _flag, when="+cuda+kokkos"+_flag)
+
+
     for _flag in list(CudaPackage.cuda_arch_values):
         depends_on("kokkos@3.3: cuda_arch=" +_flag, when="+cuda+kokkos cuda_arch=" + _flag)
         depends_on("kokkos-kernels cuda_arch=" +_flag, when="+cuda+kokkos cuda_arch=" + _flag)
@@ -67,8 +72,19 @@ class SingularityEos(CMakePackage, CudaPackage):
             self.define("SINGULARITY_TEST_SESAME", ("sesame" in self.spec.variants["build_extra"] and "tests" in self.spec)),
             self.define("SINGULARITY_BUILD_STELLARCOLLAPSE2SPINER", "stellarcollapse" in self.spec.variants["build_extra"]),
             self.define("SINGULARITY_TEST_STELLARCOLLAPSE2SPINER", ("stellarcollapse" in self.spec.variants["build_extra"] and "tests" in self.spec)),
-            self.define("SINGULARITY_USE_HDF5", True),
-            self.define("SINGULARITY_USE_EOSPAC", True)
+            self.define("SINGULARITY_USE_HDF5", "+hdf5" in self.spec),
+            self.define("SINGULARITY_USE_EOSPAC", "+eospac" in self.spec)
         ]
 
         return args
+
+    @run_after('cmake')
+    def generate_cmake_configuration(self):
+        config_fname = "singularity-eos_spackconfig.cmake"
+        cmake_config = self.cmake_args()
+        with working_dir("cmake-gen", create=True):
+            with open(config_fname, "w") as cmc:
+                for arg in cmake_config:
+                    cmc.write(arg + "\n")
+            install(config_fname, join_path(prefix, config_fname))
+        return        
