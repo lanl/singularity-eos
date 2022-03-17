4a3f199a3572052861469d2cf001940a3eea9e51
Jeff Peterson <jhp@lanl.gov>
2022-03-17 14:19:01 -0600

    Change the undef to be in the right spot

--- singularity-eos/eos/eos_base.hpp
+++ singularity-eos/eos/eos_base.hpp
@@ -26,7 +26,6 @@ static inline auto member_func_name(const char *type_name, const char *func_name
 }
 } // namespace mfuncname
 } // namespace singularity
-#undef SG_MEMBER_FUNC_NAME
 #define SG_MEMBER_FUNC_NAME()                                                            \
   singularity::mfuncname::member_func_name(typeid(CRTP).name(), __func__);
 
@@ -36,7 +35,6 @@ namespace eos_base {
 // This Macro adds the `using` statements that allow for the base class
 // vector functionality to overload the scalar implementations in the derived
 // classes
-#undef SG_ADD_BASE_CLASS_USINGS
 #define SG_ADD_BASE_CLASS_USINGS(EOSDERIVED)                                             \
   using EosBase<EOSDERIVED>::TemperatureFromDensityInternalEnergy;                       \
   using EosBase<EOSDERIVED>::InternalEnergyFromDensityTemperature;                       \
@@ -314,4 +312,5 @@ class EosBase {
 } // namespace eos_base
 } // namespace singularity
 
+#undef SG_MEMBER_FUNC_NAME
 #endif
