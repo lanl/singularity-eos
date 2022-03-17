06a7c61402a1bf540239f2630af27285ff16eb4a
Jeffrey H Peterson <jhp@lanl.gov>
2022-03-17 14:45:58 -0600

    Update copyright

--- utils/fast-math/logs.hpp
+++ utils/fast-math/logs.hpp
@@ -1,7 +1,7 @@
 //======================================================================
 // approximate log10, approximately 5x faster than std::log10
 // Author: Jonah Miller (jonahm@lanl.gov)
-// © 2021. Triad National Security, LLC. All rights reserved.  This
+// © 2022. Triad National Security, LLC. All rights reserved.  This
 // program was produced under U.S. Government contract 89233218CNA000001
 // for Los Alamos National Laboratory (LANL), which is operated by Triad
 // National Security, LLC for the U.S.  Department of Energy/National
