36da29cfa0d46067c7708f4abde7e52f975b4c20
Daniel Holladay <danl@lanl.gov>
2022-04-14 10:38:28 -0600

    Initial sap/lap ramp pressure functions.

---
+++ singularity-eos/eos/modifiers/ramps_eos.hpp
@@ -0,0 +1,163 @@
+//------------------------------------------------------------------------------
+// © 2021-2022. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S.  Department of Energy/National
+// Nuclear Security Administration. All rights in the program are
+// reserved by Triad National Security, LLC, and the U.S. Department of
+// Energy/National Nuclear Security Administration. The Government is
+// granted for itself and others acting on its behalf a nonexclusive,
+// paid-up, irrevocable worldwide license in this material to reproduce,
+// prepare derivative works, distribute copies to the public, perform
+// publicly and display publicly, and to permit others to do so.
+//------------------------------------------------------------------------------
+
+#ifndef _SINGULARITY_EOS_EOS_RAMPS_EOS_
+#define _SINGULARITY_EOS_EOS_RAMPS_EOS_
+
+#include "stdio.h"
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <utility>
+
+#include <ports-of-call/portability.hpp>
+#include <singularity-eos/base/constants.hpp>
+#include <singularity-eos/base/eos_error.hpp>
+#include <singularity-eos/eos/eos_base.hpp>
+
+namespace singularity {
+
+using namespace eos_base;
+
+template <typename T>
+class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
+ public:
+  // move semantics ensures dynamic memory comes along for the ride
+  SAPRampEOS(T &&t, const Real shift) : t_(std::forward<T>(t)), shift_(shift) {}
+  SAPRampEOS() = default;
+
+  auto GetOnDevice() { return SAPRampEOS<T>(t_.GetOnDevice(), shift_); }
+  inline void Finalize() { t_.Finalize(); }
+
+  PORTABLE_INLINE_FUNCTION
+  Real get_ramp_pressure(Real rho) const {
+    const Real p_ramp {rho < rholow ? 0.0              :
+                       rho < rhomid ? a*(rho/r0 - 1.0) :
+                                      b*(rho/r0 - c)};
+    return p_ramp;
+  }
+
+  PORTABLE_FUNCTION
+  Real TemperatureFromDensityInternalEnergy(const Real rho, const Real sie,
+                                            Real *lambda = nullptr) const {
+    return t_.TemperatureFromDensityInternalEnergy(rho, sie, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real InternalEnergyFromDensityTemperature(const Real rho, const Real temperature,
+                                            Real *lambda = nullptr) const {
+    return t_.InternalEnergyFromDensityTemperature(rho, temperature, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real PressureFromDensityInternalEnergy(const Real rho, const Real sie,
+                                         Real *lambda = nullptr) const {
+    // ramp pressure
+    const Real p_ramp {get_ramp_pressure(rho)};
+    // pressure from eos
+    const Real p_eos {t_.PressureFromDensityInternalEnergy(rho, sie, lambda)};
+    // return max(p_ramp, p_eos)
+    return p_eos < p_ramp ? p_ramp : p_eos;
+  }
+  PORTABLE_FUNCTION
+  Real SpecificHeatFromDensityInternalEnergy(const Real rho, const Real sie,
+                                             Real *lambda = nullptr) const {
+    return t_.SpecificHeatFromDensityInternalEnergy(rho, sie, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real BulkModulusFromDensityInternalEnergy(const Real rho, const Real sie,
+                                            Real *lambda = nullptr) const {
+    return t_.BulkModulusFromDensityInternalEnergy(rho, sie, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real GruneisenParamFromDensityInternalEnergy(const Real rho, const Real sie,
+                                               Real *lambda = nullptr) const {
+    return t_.GruneisenParamFromDensityInternalEnergy(rho, sie, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real PressureFromDensityTemperature(const Real rho, const Real temperature,
+                                      Real *lambda = nullptr) const {
+    const Real p_ramp {get_ramp_pressure(rho)};
+    const Real p_eos {t_.PressureFromDensityTemperature(rho, temperature, lambda)};
+    return p_eos < p_ramp ? p_ramp : p_eos;
+  }
+  PORTABLE_FUNCTION
+  Real SpecificHeatFromDensityTemperature(const Real rho, const Real temperature,
+                                          Real *lambda = nullptr) const {
+    return t_.SpecificHeatFromDensityTemperature(rho, temperature, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real BulkModulusFromDensityTemperature(const Real rho, const Real temperature,
+                                         Real *lambda = nullptr) const {
+    return t_.BulkModulusFromDensityTemperature(rho, temperature, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real GruneisenParamFromDensityTemperature(const Real rho, const Real temperature,
+                                            Real *lambda = nullptr) const {
+    return t_.GruneisenParamFromDensityTemperature(rho, temperature, lambda);
+  }
+  PORTABLE_FUNCTION
+  void FillEos(Real &rho, Real &temp, Real &energy, Real &press, Real &cv, Real &bmod,
+               const unsigned long output, Real *lambda = nullptr) const {
+    Real senergy;
+    switch (t_.PreferredInput()) {
+    case thermalqs::density | thermalqs::temperature:
+      t_.FillEos(rho, temp, energy, press, cv, bmod, output, lambda);
+      energy = energy + shift_;
+      break;
+    case thermalqs::density | thermalqs::specific_internal_energy:
+      senergy = energy - shift_;
+      t_.FillEos(rho, temp, senergy, press, cv, bmod, output, lambda);
+      break;
+    default:
+      EOS_ERROR("Didn't find a valid input for SAPRampEOS::FillEOS\n");
+    }
+  }
+
+  PORTABLE_INLINE_FUNCTION
+  int nlambda() const noexcept { return t_.nlambda(); }
+
+  PORTABLE_FUNCTION
+  unsigned long PreferredInput() const { return t_.PreferredInput(); }
+
+  PORTABLE_FUNCTION void PrintParams() const {
+    t_.PrintParams();
+    printf("scaling_ratio = %f\n", shift_);
+  }
+  PORTABLE_FUNCTION
+  void DensityEnergyFromPressureTemperature(const Real press, const Real temp,
+                                            Real *lambda, Real &rho, Real &sie) const {
+    t_.DensityEnergyFromPressureTemperature(press, temp, lambda, rho, sie);
+    sie = sie + shift_;
+  }
+
+  PORTABLE_FUNCTION
+  void ValuesAtReferenceState(Real &rho, Real &temp, Real &sie, Real &press, Real &cv,
+                              Real &bmod, Real &dpde, Real &dvdt,
+                              Real *lambda = nullptr) const {
+    t_.ValuesAtReferenceState(rho, temp, sie, press, cv, bmod, dpde, dvdt, lambda);
+    sie += shift_;
+  }
+
+  // Vector functions that overload the scalar versions declared here.
+  SG_ADD_BASE_CLASS_USINGS(SAPRampEOS<T>)
+
+ private:
+  T t_;
+  double shift_;
+};
+
+} // namespace singularity
+
+#endif // _SINGULARITY_EOS_EOS_RAMPS_EOS_
