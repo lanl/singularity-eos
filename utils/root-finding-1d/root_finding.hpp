652773a39a0a4959c7c8958907ddb6d75167b555
Christopher Mauney <mauneyc@lanl.gov>
2022-04-21 12:00:31 -0600

    make format

--- utils/root-finding-1d/root_finding.hpp
+++ utils/root-finding-1d/root_finding.hpp
@@ -31,9 +31,9 @@
 
 #define SINGULARITY_ROOT_DEBUG (0)
 #define SINGULARITY_ROOT_VERBOSE (0)
-//TODO: mauneyc: this isn't used here, and I can't
-//find it used elsewhere. it also doesn't get unset.
-//should it be ditched?
+// TODO: mauneyc: this isn't used here, and I can't
+// find it used elsewhere. it also doesn't get unset.
+// should it be ditched?
 #define SINGULARITY_ROOT_NAN_OK (0)
 
 #define SINGULARITY_MY_SIGN(x) (x > 0) - (x < 0)
