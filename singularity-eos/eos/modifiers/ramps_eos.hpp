c35a76d758510f09d343a33fe1957258372920da
Daniel Holladay <dholladay00@users.noreply.github.com>
2022-04-27 11:08:12 -0600

    Finished draft implementation of the sap ramp. Naming may need to change since it it taken from lap.

--- singularity-eos/eos/modifiers/ramps_eos.hpp
+++ singularity-eos/eos/modifiers/ramps_eos.hpp
@@ -36,7 +36,14 @@ template <typename T>
 class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
  public:
   // move semantics ensures dynamic memory comes along for the ride
-  SAPRampEOS(T &&t, const Real shift) : t_(std::forward<T>(t)), shift_(shift) {}
+  SAPRampEOS(T &&t, const Real r0, const Real a, const Real b, const Real c) 
+  : t_(std::forward<T>(t)),
+    r0_(r0),
+    a_(a),
+    b_(b),
+    c_(c),
+    rmid_(r0*(a-b*c)/(a-b))
+    {}
   SAPRampEOS() = default;
 
   auto GetOnDevice() { return SAPRampEOS<T>(t_.GetOnDevice(), shift_); }
@@ -44,9 +51,9 @@ class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
 
   PORTABLE_INLINE_FUNCTION
   Real get_ramp_pressure(Real rho) const {
-    const Real p_ramp {rho < rholow ? 0.0              :
-                       rho < rhomid ? a*(rho/r0 - 1.0) :
-                                      b*(rho/r0 - c)};
+    const Real p_ramp {rho < r0_ ? 0.0                  :
+                       rho < rmid_ ? a_*(rho/r0_ - 1.0) :
+                                       b_*(rho/r0_ - c_)};
     return p_ramp;
   }
 
@@ -110,19 +117,23 @@ class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
   PORTABLE_FUNCTION
   void FillEos(Real &rho, Real &temp, Real &energy, Real &press, Real &cv, Real &bmod,
                const unsigned long output, Real *lambda = nullptr) const {
-    Real senergy;
-    switch (t_.PreferredInput()) {
-    case thermalqs::density | thermalqs::temperature:
-      t_.FillEos(rho, temp, energy, press, cv, bmod, output, lambda);
-      energy = energy + shift_;
-      break;
-    case thermalqs::density | thermalqs::specific_internal_energy:
-      senergy = energy - shift_;
-      t_.FillEos(rho, temp, senergy, press, cv, bmod, output, lambda);
-      break;
-    default:
-      EOS_ERROR("Didn't find a valid input for SAPRampEOS::FillEOS\n");
+    // density must be an input
+    assert(!(output & thermalqs::density));
+    // output passed into internal filleos can't include pressure
+    const unsigned long ramp_out = output & ~thermalqs::pressure;
+    // if pressure is output, calculate it first
+    if (output & thermalqs::pressure) {
+      // maybe switch case on preferred input and check for not output of the input
+      // for now sie lookup is prioritized
+      if (!(output & thermalqs::specific_internal_energy)) {
+	press = this->PressureFromDensityInternalEnergy(rho, energy, lambda);
+      }
+      else if (!(output & thermalqs::temperature)) {
+	press = this->PressureFromDensityTemperature(rho, temp, lambda);
+      }
     }
+    // call internal filleos
+    t_.FillEos(rho, temp, energy, press, cv, bmod, ramp_out, lambda);
   }
 
   PORTABLE_INLINE_FUNCTION
@@ -133,13 +144,12 @@ class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
 
   PORTABLE_FUNCTION void PrintParams() const {
     t_.PrintParams();
-    printf("scaling_ratio = %f\n", shift_);
+    printf("r0=%e\na=%e\nb=%e\nc=%e\n", r0_, a_, b_, c_);
   }
   PORTABLE_FUNCTION
   void DensityEnergyFromPressureTemperature(const Real press, const Real temp,
                                             Real *lambda, Real &rho, Real &sie) const {
     t_.DensityEnergyFromPressureTemperature(press, temp, lambda, rho, sie);
-    sie = sie + shift_;
   }
 
   PORTABLE_FUNCTION
@@ -147,7 +157,6 @@ class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
                               Real &bmod, Real &dpde, Real &dvdt,
                               Real *lambda = nullptr) const {
     t_.ValuesAtReferenceState(rho, temp, sie, press, cv, bmod, dpde, dvdt, lambda);
-    sie += shift_;
   }
 
   // Vector functions that overload the scalar versions declared here.
@@ -155,7 +164,11 @@ class SAPRampEOS : public EosBase<SAPRampEOS<T>> {
 
  private:
   T t_;
-  double shift_;
+  Real r0_;
+  Real a_;
+  Real b_;
+  Real c_;
+  Real rmid_;
 };
 
 } // namespace singularity
