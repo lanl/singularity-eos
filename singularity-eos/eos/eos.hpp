404adfd474291f95bf68482d12b7361151f33f63
Christopher Mauney <mauneyc@lanl.gov>
2022-06-14 10:56:22 -0600

    formated update

--- singularity-eos/eos/eos.hpp
+++ singularity-eos/eos/eos.hpp
@@ -735,10 +735,18 @@ class SpinerEOSDependsRhoSie : public EosBase<SpinerEOSDependsRhoSie> {
   PORTABLE_INLINE_FUNCTION Real lEOffset() const { return lEOffset_; }
   PORTABLE_INLINE_FUNCTION Real rhoMin() const { return fromLog_(lRhoMin_, lRhoOffset_); }
   PORTABLE_INLINE_FUNCTION Real rhoMax() const { return fromLog_(lRhoMax_, lRhoOffset_); }
-  PORTABLE_INLINE_FUNCTION Real TMin() const { return fromLog_(T_.range(0).min(), lTOffset_); }
-  PORTABLE_INLINE_FUNCTION Real TMax() const { return fromLog_(T_.range(0).max(), lTOffset_); }
-  PORTABLE_INLINE_FUNCTION Real sieMin() const { return fromLog_(sie_.range(0).min(), lEOffset_); }
-  PORTABLE_INLINE_FUNCTION Real sieMax() const { return fromLog_(sie_.range(0).max(), lEOffset_); }
+  PORTABLE_INLINE_FUNCTION Real TMin() const {
+    return fromLog_(T_.range(0).min(), lTOffset_);
+  }
+  PORTABLE_INLINE_FUNCTION Real TMax() const {
+    return fromLog_(T_.range(0).max(), lTOffset_);
+  }
+  PORTABLE_INLINE_FUNCTION Real sieMin() const {
+    return fromLog_(sie_.range(0).min(), lEOffset_);
+  }
+  PORTABLE_INLINE_FUNCTION Real sieMax() const {
+    return fromLog_(sie_.range(0).max(), lEOffset_);
+  }
 
   PORTABLE_FORCEINLINE_FUNCTION Real MinimumDensity() const { return rhoMin(); }
   PORTABLE_FORCEINLINE_FUNCTION Real MinimumTemperature() const { return TMin(); }
@@ -1227,9 +1235,7 @@ class EOSPAC : public EosBase<EOSPAC> {
   PORTABLE_INLINE_FUNCTION int nlambda() const noexcept { return 0; }
   inline void Finalize() {}
   static std::string EosType() { return std::string("EOSPAC"); }
-  inline void PrintParams() const {
-    printf("EOSPAC parameters:\nmatid = %s\n", matid_);
-  }
+  inline void PrintParams() const { printf("EOSPAC parameters:\nmatid = %s\n", matid_); }
   PORTABLE_FORCEINLINE_FUNCTION Real MinimumDensity() const { return rho_min_; }
   PORTABLE_FORCEINLINE_FUNCTION Real MinimumTemperature() const { return temp_min_; }
 
