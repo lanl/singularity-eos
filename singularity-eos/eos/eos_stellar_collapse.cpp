e0a280fc4a81b5d3d592d274e69359ad23ba9fd4
Richard Berger <rberger@lanl.gov>
2022-06-16 10:18:37 -0600

    Formatting

--- singularity-eos/eos/eos_stellar_collapse.cpp
+++ singularity-eos/eos/eos_stellar_collapse.cpp
@@ -62,9 +62,7 @@ namespace singularity {
 constexpr char METADATA_NAME[] = "Metadata";
 
 StellarCollapse::StellarCollapse(const std::string &filename, bool use_sp5,
-                                 bool filter_bmod)
-{
-
+                                 bool filter_bmod) {
   if (use_sp5) {
     LoadFromSP5File_(filename);
   } else {
