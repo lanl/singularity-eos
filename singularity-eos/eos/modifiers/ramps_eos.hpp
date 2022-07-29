731d5efe50ae7e3004d2724c51fc78632916d4ca
Richard Berger <rberger@lanl.gov>
2022-07-29 16:47:01 -0600

    python: add bilinear ramp modifier

--- singularity-eos/eos/modifiers/ramps_eos.hpp
+++ singularity-eos/eos/modifiers/ramps_eos.hpp
@@ -88,6 +88,9 @@ class BilinearRampEOS : public EosBase<BilinearRampEOS<T>> {
   }
   BilinearRampEOS() = default;
 
+  // give me std::format or fmt::format...
+  static std::string EosType() { return std::string("BilinearRampEOS<") + T::EosType() + std::string(">"); }
+
   auto GetOnDevice() { return BilinearRampEOS<T>(t_.GetOnDevice(), r0_, a_, b_, c_); }
   inline void Finalize() { t_.Finalize(); }
 
@@ -210,8 +213,7 @@ class BilinearRampEOS : public EosBase<BilinearRampEOS<T>> {
   PORTABLE_INLINE_FUNCTION
   int nlambda() const noexcept { return t_.nlambda(); }
 
-  PORTABLE_FUNCTION
-  unsigned long PreferredInput() const { return t_.PreferredInput(); }
+  static constexpr unsigned long PreferredInput() { return T::PreferredInput(); }
 
   PORTABLE_FUNCTION void PrintParams() const {
     t_.PrintParams();
