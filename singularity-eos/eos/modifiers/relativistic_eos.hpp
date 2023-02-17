44a9529d04036bdd40c198fcf449265c239c8524
Jeffrey H Peterson <jhp@lanl.gov>
2023-02-16 19:10:12 -0700

    Add basic, unmodified, entropy lookups for ramps and relativistic modifiers

--- singularity-eos/eos/modifiers/relativistic_eos.hpp
+++ singularity-eos/eos/modifiers/relativistic_eos.hpp
@@ -92,6 +92,11 @@ class RelativisticEOS : public EosBase<RelativisticEOS<T>> {
     return t_.PressureFromDensityInternalEnergy(rho, sie, lambda);
   }
   PORTABLE_FUNCTION
+  Real EntropyFromDensityInternalEnergy(const Real rho, const Real sie,
+                                         Real *lambda = nullptr) const {
+    return t_.EntropyFromDensityInternalEnergy(rho, sie, lambda);
+  }
+  PORTABLE_FUNCTION
   Real SpecificHeatFromDensityInternalEnergy(const Real rho, const Real sie,
                                              Real *lambda = nullptr) const {
     return t_.SpecificHeatFromDensityInternalEnergy(rho, sie, lambda);
@@ -113,6 +118,11 @@ class RelativisticEOS : public EosBase<RelativisticEOS<T>> {
   Real PressureFromDensityTemperature(const Real rho, const Real temperature,
                                       Real *lambda = nullptr) const {
     return t_.PressureFromDensityTemperature(rho, temperature, lambda);
+  }
+  PORTABLE_FUNCTION
+  Real EntropyFromDensityTemperature(const Real rho, const Real temperature,
+                                         Real *lambda = nullptr) const {
+    return t_.EntropyFromDensityTemperature(rho, temperature, lambda);
   }
   PORTABLE_FUNCTION
   Real SpecificHeatFromDensityTemperature(const Real rho, const Real temperature,
