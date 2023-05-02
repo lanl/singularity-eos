6bce67420b3738f99ffe3da80cca76ebf41980a2
Christopher Mauney <mauneyc@pn2305185.lanl.gov>
2023-05-02 17:06:54 -0600

    options and consistancy checks

--- cmake/singularity/hdf5.cmake
+++ cmake/singularity/hdf5.cmake
@@ -7,14 +7,15 @@ macro(singularity_enable_hdf5 target)
   endif()
 
   if(NOT HDF5_LIBRARIES)
-    find_package(HDF5 COMPONENTS HL REQUIRED)
+    find_package(HDF5 COMPONENTS C HL REQUIRED)
   endif()
 
   target_include_directories(${target} SYSTEM PUBLIC ${HDF5_INCLUDE_DIRS})
   target_link_libraries(${target} PUBLIC ${HDF5_LIBRARIES})
 
   if(HDF5_IS_PARALLEL)
-    find_package(MPI COMPONENTS CXX REQUIRED)
+    enable_language(C)
+    find_package(MPI COMPONENTS C CXX REQUIRED)
     target_link_libraries(${target} PUBLIC MPI::MPI_CXX)
   endif()
 
