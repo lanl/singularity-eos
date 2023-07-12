4adceb476e0c6b2d08d4c6b19e24a2d3d989cb53
Christopher Michael Mauney <mauneyc@lanl.gov>
2023-07-12 14:07:18 -0600

    update spiner spackage to accecpt lesser cmake version

--- spack-repo/packages/spiner/package.py
+++ spack-repo/packages/spiner/package.py
@@ -50,7 +50,7 @@ class Spiner(CMakePackage, CudaPackage):
     variant("python", default=False, description="Python, Numpy & Matplotlib Support")
 
     depends_on("cmake@3.12:", when="@:1.5.1")
-    depends_on("cmake@3.23:", when="@1.6.0:")
+    depends_on("cmake@3.19:", when="@1.6.0:")
     depends_on("catch2@2.13.4:2.13.9")
     depends_on("ports-of-call@1.2.0:", when="@:1.5.1")
     depends_on("ports-of-call@1.5.1:", when="@1.6.0:")
