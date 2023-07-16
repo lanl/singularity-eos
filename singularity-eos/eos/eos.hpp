95e1be9fb797d8ca17cfb99f23f3107d7dada066
Gopinath Subramanian <gss@lanl.gov>
2023-07-16 14:15:13 -0600

    Changed registered name of polynomial eos to sap_polynomial

--- singularity-eos/eos/eos.hpp
+++ singularity-eos/eos/eos.hpp
@@ -38,7 +38,7 @@
 #include <singularity-eos/eos/eos_gruneisen.hpp>
 #include <singularity-eos/eos/eos_ideal.hpp>
 #include <singularity-eos/eos/eos_jwl.hpp>
-#include <singularity-eos/eos/eos_polynomial.hpp>
+#include <singularity-eos/eos/eos_sap_polynomial.hpp>
 #include <singularity-eos/eos/eos_spiner.hpp>
 #include <singularity-eos/eos/eos_stellar_collapse.hpp>
 #include <singularity-eos/eos/eos_vinet.hpp>
@@ -64,7 +64,7 @@ using singularity::detail::transform_variadic_list;
 
 // all eos's
 static constexpr const auto full_eos_list =
-    tl<IdealGas, Gruneisen, Vinet, JWL, DavisReactants, DavisProducts, Polynomial
+    tl<IdealGas, Gruneisen, Vinet, JWL, DavisReactants, DavisProducts, SAP_Polynomial
 #ifdef SPINER_USE_HDF
        ,
        SpinerEOSDependsRhoT, SpinerEOSDependsRhoSie, StellarCollapse
