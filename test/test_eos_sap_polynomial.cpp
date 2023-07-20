2f2b93b26f7119d2e5ad2877791a0e806eb8ada8
Gopinath Subramanian <gss@lanl.gov>
2023-07-20 10:58:49 -0600

    Added parameters to test

--- test/test_eos_sap_polynomial.cpp
+++ test/test_eos_sap_polynomial.cpp
@@ -28,24 +28,25 @@
 using singularity::EOS;
 using singularity::SAP_Polynomial;
 
-SCENARIO("Aluminium SAP_Polynomial EOS", "Check if eos returns expected values") {
+SCENARIO("SAP_Polynomial EOS", "Check if eos returns expected values") {
   GIVEN("Parameters for a SAP_Polynomial EOS") {
     // Unit conversions
-    constexpr Real cm = 1.;
-    constexpr Real us = 1e-06;
+    constexpr Real ubar_per_Mbar = 1e12;
     constexpr Real Mbcc_per_g = 1e12;
 
-    constexpr Real rho0 = 2.7; // g/cc
-    constexpr Real a0  = 0.0;
-    constexpr Real a1  = 0.73 * cm / us;
-    constexpr Real a2c = 0.73 * cm / us;
-    constexpr Real a2e = 0.73 * cm / us;
-    constexpr Real a3  = 0.73 * cm / us;
-    constexpr Real b0  = 0.0;
-    constexpr Real b1  = 0.0;
-    constexpr Real b2c = 0.0;
-    constexpr Real b2e = 0.0;
-    constexpr Real b3  = 0.0;
+    // Constants of the form 0.0? are zero for the original eos
+    constexpr Real rho0 = 16.6; // g/cc
+    constexpr Real a0   = 0.01 * ubar_per_Mbar;
+    constexpr Real a1   = 1.09 * ubar_per_Mbar;
+    constexpr Real a2c  = 0.90 * ubar_per_Mbar;
+    constexpr Real a2e  = 0.02 * ubar_per_Mbar;
+    constexpr Real a3   = 1.80 * ubar_per_Mbar;
+    constexpr Real b0   = 1.90;
+    constexpr Real b1   = 0.01;
+    constexpr Real b2c  = 0.02;
+    constexpr Real b2e  = 0.03;
+    constexpr Real b3   = 0.04;
+
     // Create the EOS
     EOS host_eos = SAP_Polynomial(rho0, a0, a1, a2c, a2e, a3, b0, b1, b2c, b2e, b3);
     EOS eos = host_eos.GetOnDevice();
