47790a504c9df822b70b3688e8f56c378e86395c
jdolence <jcdolence@gmail.com>
2023-10-10 11:34:22 -0600

    formatting

--- singularity-eos/closure/mixed_cell_models.hpp
+++ singularity-eos/closure/mixed_cell_models.hpp
@@ -273,9 +273,9 @@ class PTESolverBase {
   }
 
   template <typename EOS_t>
-  PORTABLE_FORCEINLINE_FUNCTION
-  static Real GetPressureFromPreferred(const EOS_t &eos, const Real rho, const Real T,
-                                       Real sie, Real *lambda, const bool do_e_lookup) {
+  PORTABLE_FORCEINLINE_FUNCTION static Real
+  GetPressureFromPreferred(const EOS_t &eos, const Real rho, const Real T, Real sie,
+                           Real *lambda, const bool do_e_lookup) {
     Real P{};
     if (eos.PreferredInput() ==
         (thermalqs::density | thermalqs::specific_internal_energy)) {
