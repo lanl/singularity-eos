ec7891a188f6e40f4a02fa4d658dbc548be828c8
Jonah Maxwell Miller <jonahm@lanl.gov>
2023-10-10 18:43:46 -0600

    oops

--- test/test_eos_unit.cpp
+++ test/test_eos_unit.cpp
@@ -518,9 +518,9 @@ class CheckPofRE {
   template <typename T>
   PORTABLE_FUNCTION
   void operator()(const T &eos) {
-    int *P = P_;
-    int *rho = rho_;
-    int *sie = sie_;
+    Real *P = P_;
+    Real *rho = rho_;
+    Real *sie = sie_;
     portableReduce(
         "MyCheckPofRE", 0, N_,
         PORTABLE_LAMBDA(const int i, int &nw) {
