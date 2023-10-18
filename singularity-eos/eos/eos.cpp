790f176d9c29dcb0dea8674508eef17114d2655f
Jonah Miller <jonahm@lanl.gov>
2023-10-18 13:48:03 -0600

    typo

--- singularity-eos/eos/eos.cpp
+++ singularity-eos/eos/eos.cpp
@@ -24,7 +24,7 @@
 // wrong:
 // 1. CMake will be unable to infer a proper linker
 //    so one must be specified by hand.
-// 2. no dynamic library file (e.g., "*.a" will be generated,
+// 2. no dynamic library file (e.g., "*.a") will be generated,
 //    but dependencies that link against it, such as another library,
 //    or in our case, tests, will look for it. This will lead to
 //    a failure of the downstream library at link time.
