916f45045a9a0d9a0c5975e9ac2b98ff1b43b265
Seth James Gerberding <gerberding@ro-rfe3.lanl.gov>
2024-07-30 10:57:04 -0600

    Initial commit for StiffGas python interface

--- python/module.cpp
+++ python/module.cpp
@@ -32,6 +32,11 @@ PYBIND11_MODULE(singularity_eos, m) {
       py::arg("gm1"), py::arg("Cv")
     );
 
+  eos_class<StiffGas>(m, "StiffGas")
+  .def(py::init())
+  .def(py::init<Real, Real, Real, Real>(), py::arg("gm1"), py::arg("Cv"), py::arg("q"), py:;arg("Pinf"));
+
+
   eos_class<Gruneisen>(m, "Gruneisen")
     .def(py::init())
     .def(
