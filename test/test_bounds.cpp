b384d2c933f78914fa003a36de9e47a668f74a5d
Jonah Miller <jonahm@lanl.gov>
2024-11-18 09:45:32 -0700

    useless error messages... and it works on my machine...

--- test/test_bounds.cpp
+++ test/test_bounds.cpp
@@ -79,6 +79,8 @@ SCENARIO("Logarithmic, single-grid bounds in the bounds object", "[Bounds]") {
         int ianchor;
         Spiner::weights_t<Real> w;
         lRhoBounds.grid.weights(lanchor, ianchor, w);
+        printf("%.14e, %.14e, %.14e\n",
+               std::abs(w[0] - 1), std::abs(w[1]), REAL_TOL);
         REQUIRE(std::abs(w[0] - 1) <= REAL_TOL);
         REQUIRE(std::abs(w[1]) <= REAL_TOL);
       }
