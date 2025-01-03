8cc2508b0a39314158c45a2cd633272492b6254c
Jonah Miller <jonahm@lanl.gov>
2025-01-03 12:27:43 -0700

    formatting

--- test/eos_unit_test_helpers.hpp
+++ test/eos_unit_test_helpers.hpp
@@ -157,8 +157,8 @@ CheckRhoSieFromPT(EOS eos, Real rho, Real T,
   Real residual = P_test - P;
   Real frac_residual = robust::ratio(residual, P);
   bool results_good = (isClose(rho, rtest, 1e-8) && isClose(sie, etest, 1e-8))
-    // This is as good as it will get sometimes.
-    || (std::abs(frac_residual) < 10*robust::EPS());
+                      // This is as good as it will get sometimes.
+                      || (std::abs(frac_residual) < 10 * robust::EPS());
   if (!results_good) {
     printf("RhoSie of PT failure!\n"
            "\trho_true = %.14e\n"
