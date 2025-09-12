02a7396a7143f025b79f0e41c05dce295fc80bd1
Jeffrey H Peterson <jhp@lanl.gov>
2025-09-12 11:46:01 -0600

    Add safeSet() and safeGet() helpers

--- singularity-eos/base/indexable_types.hpp
+++ singularity-eos/base/indexable_types.hpp
@@ -24,8 +24,85 @@
 
 namespace singularity {
 namespace IndexerUtils {
-// Convenience function for accessing an indexer by either type or
-// natural number index depending on what is available
+
+// Identifies an indexer as a type-based indexer
+template<class, class=void>
+struct is_type_indexer : std::false_type {};
+template<class Indexer_t>
+struct is_type_indexer<Indexer_t,
+  std::void_t<decltype(std::decay_t<Indexer_t>::is_type_indexable)>>
+  : std::bool_constant<std::decay_t<Indexer_t>::is_type_indexable> {};
+template<class Indexer_t>
+constexpr bool is_type_indexer_v = is_type_indexer<Indexer_t>::value;
+
+// The "safe" version of Get(). This function will ONLY return a value IF that
+// type-based index is present in the Indexer OR if the Indexer doesn't support
+// type-based indexing.
+template <typename T, typename Indexer_t>
+inline bool safeGet(Indexer_t const& lambda, std::size_t const idx, Real& out) noexcept {
+  // If null then nothing happens
+  if (variadic_utils::is_nullptr(lambda)) {
+    return false;
+  }
+
+  // Return value if type index is available
+  if constexpr (variadic_utils::is_indexable_v<Indexer_t, T>) {
+    out = lambda[T{}];
+    return true;
+  }
+
+  // Do nothing if lambda has type indexing BUT doesn't have this type index
+  if constexpr (is_type_indexer_v<Indexer_t>) {
+    return false;
+  }
+
+  // Fall back to numerical indexing if no type indexing
+  if constexpr (variadic_utils::has_whole_num_index<Indexer_t>::value) {
+    out = lambda[idx];
+    return true;
+  }
+
+  // Something else...
+  return false;
+}
+
+// Break out "Set" functionality from "Get". The original "Get()" did both, but
+// the "safe" version needs to separate that functionality for setting the
+// values in a lambda
+template <typename T, typename Indexer_t>
+inline bool safeSet(Indexer_t& lambda, std::size_t const idx, Real const in) noexcept {
+  // If null then nothing happens
+  if (variadic_utils::is_nullptr(lambda)) {
+    return false;
+  }
+
+  // Return value if type index is available
+  if constexpr (variadic_utils::is_indexable_v<Indexer_t, T>) {
+    lambda[T{}] = in;
+    return true;
+  }
+
+  // Do nothing if lambda has type indexing BUT doesn't have this type index
+  if constexpr (is_type_indexer_v<Indexer_t>) {
+    return false;
+  }
+
+  // Fall back to numerical indexing if no type indexing
+  if constexpr (variadic_utils::has_whole_num_index<Indexer_t>::value) {
+    lambda[idx] = in;
+    return true;
+  }
+
+  // Something else...
+  return false;
+}
+
+// NOTE: this Get is "unsafe" because it can allow you to overwrite a type-based
+// index since it automatically falls back to numeric indexing if the type
+// index isn't present.
+
+// Convenience function for accessing an indexer by either type or natural
+// number index depending on what is available.
 template <typename T, typename Indexer_t>
 PORTABLE_FORCEINLINE_FUNCTION auto &Get(Indexer_t &&lambda, std::size_t idx = 0) {
   if constexpr (variadic_utils::is_indexable_v<Indexer_t, T>) {
@@ -40,25 +117,44 @@ PORTABLE_FORCEINLINE_FUNCTION auto &Get(Indexer_t &&lambda, std::size_t idx = 0)
 template <typename Data_t, typename... Ts>
 class VariadicIndexerBase {
  public:
+  // Any class that wants to be recognized as indexable (so that we don't
+  // accidentally fall back to integer indexing when we don't want to) needs to
+  // include this.
+  constexpr static bool is_type_indexable = true;
+
+  // JHP: another option for the `is_type_indexable` flag is to take the ADL
+  // route. Essentially this would involve defining a friend function that
+  // could be defined in an appropriate namesapce so that theoretically a host
+  // code could use a TPL container with type-based indexing and allow that
+  // container to be flagged in our code as acceptable. This seems like a bit
+  // of a heavy hammer for what we need here though. We can easily change this
+  // if a TPL provides a type that is being used for this purpose.
+
   VariadicIndexerBase() = default;
+
   PORTABLE_FORCEINLINE_FUNCTION
   VariadicIndexerBase(const Data_t &data) : data_(data) {}
+
   template <typename T,
             typename = std::enable_if_t<variadic_utils::contains<T, Ts...>::value>>
   PORTABLE_FORCEINLINE_FUNCTION Real &operator[](const T &t) {
     constexpr std::size_t idx = variadic_utils::GetIndexInTL<T, Ts...>();
     return data_[idx];
   }
+
   PORTABLE_FORCEINLINE_FUNCTION
   Real &operator[](const std::size_t idx) { return data_[idx]; }
+
   template <typename T,
             typename = std::enable_if_t<variadic_utils::contains<T, Ts...>::value>>
   PORTABLE_FORCEINLINE_FUNCTION const Real &operator[](const T &t) const {
     constexpr std::size_t idx = variadic_utils::GetIndexInTL<T, Ts...>();
     return data_[idx];
   }
+
   PORTABLE_FORCEINLINE_FUNCTION
   const Real &operator[](const std::size_t idx) const { return data_[idx]; }
+
   static inline constexpr std::size_t size() { return sizeof...(Ts); }
 
  private:
@@ -70,6 +166,7 @@ using VariadicIndexer = VariadicIndexerBase<std::array<Real, sizeof...(Ts)>, Ts.
 // uses a Real*
 template <typename... Ts>
 using VariadicPointerIndexer = VariadicIndexerBase<Real *, Ts...>;
+
 } // namespace IndexerUtils
 
 namespace IndexableTypes {
--- test/test_indexable_types.cpp
+++ test/test_indexable_types.cpp
@@ -29,14 +29,22 @@ using Lambda_t = VariadicIndexer<MeanIonizationState, ElectronFraction, LogDensi
 
 class ManualLambda_t {
  public:
+  constexpr static size_t length = 3;
   ManualLambda_t() = default;
+  const Real &operator[](const std::size_t idx) const { return data_[idx]; }
   Real &operator[](const std::size_t idx) { return data_[idx]; }
   Real &operator[](const MeanIonizationState &zbar) { return data_[0]; }
   Real &operator[](const ElectronFraction &Ye) { return data_[1]; }
   Real &operator[](const LogDensity &lRho) { return data_[2]; }
 
  private:
-  std::array<Real, 3> data_;
+  std::array<Real, length> data_;
+};
+
+class NewManualLambda_t : public ManualLambda_t {
+ public:
+  // Enable recognition that this is type-indexable
+  constexpr static bool is_type_indexable = true;
 };
 
 SCENARIO("IndexableTypes and VariadicIndexer", "[IndexableTypes][VariadicIndexer]") {
@@ -73,13 +81,73 @@ SCENARIO("IndexableTypes and VariadicIndexer", "[IndexableTypes][VariadicIndexer
         REQUIRE(lRho == static_cast<Real>(2));
       }
     }
+    WHEN("We use the safeGet functionality") {
+      constexpr Real unmodified = -1.0;
+      Real destination = unmodified;
+      WHEN("We request a type that exists") {
+        const bool modified = safeGet<MeanIonizationState>(lambda, 2, destination);
+        THEN("The destination value will be modified") {
+          CHECK(modified);
+          REQUIRE(destination == lambda[MeanIonizationState{}]);
+        }
+      }
+      WHEN("We request a type that doesn't exist") {
+        const bool modified = safeGet<TableStatus>(lambda, 2, destination);
+        THEN("The destination value will remain UNmodified") {
+          CHECK(!modified);
+          REQUIRE(destination == unmodified);
+        }
+      }
+      WHEN("A normal array-like lambda is used") {
+        std::array<Real, Lambda_t::size()> lambda_arr{1, 2, 3};
+        constexpr size_t my_index = 2;
+        const bool modified = safeGet<LogDensity>(lambda_arr, my_index, destination);
+        THEN("The destination value will reflect the index from the array") {
+          CHECK(modified);
+          REQUIRE(destination == lambda_arr[my_index]);
+        }
+      }
+    }
+    WHEN("We use the safeSet functionality") {
+      constexpr Real new_value = -1.0;
+      WHEN("We want to set a value for a type index that exists") {
+        const bool modified = safeSet<MeanIonizationState>(lambda, 2, new_value);
+        THEN("The lambda index was modified") {
+          CHECK(modified);
+          REQUIRE(lambda[MeanIonizationState{}] == new_value);
+        }
+      }
+      WHEN("We want to set a value for a type index that doesn't exist") {
+        const bool modified = safeSet<TableStatus>(lambda, 2, new_value);
+        Lambda_t old_lambda;
+        for (std::size_t i = 0; i < Lambda_t::size(); ++i) {
+          old_lambda[i] = lambda[i];
+        }
+        THEN("None of the lambda values were modified") {
+          CHECK(!modified);
+          for (std::size_t i = 0; i < Lambda_t::size(); ++i) {
+            INFO("i: " << i);
+            CHECK(lambda[i] == old_lambda[i]);
+          }
+        }
+      }
+      WHEN("A normal array-like lambda is used") {
+        std::array<Real, Lambda_t::size()> lambda_arr{4, 5, 6};
+        constexpr size_t my_index = 1;
+        const bool modified = safeSet<LogDensity>(lambda_arr, my_index, new_value);
+        THEN("The lambda value at the appropriate index has been modified") {
+          CHECK(modified);
+          REQUIRE(lambda_arr[my_index] == new_value);
+        }
+      }
+    }
   }
 }
 
 SCENARIO("IndexableTypes and ManualLambda", "[IndexableTypes]") {
   GIVEN("A manually written indexer, filled with indices 0, 1, 2") {
     ManualLambda_t lambda;
-    for (std::size_t i = 0; i < 3; ++i) {
+    for (std::size_t i = 0; i < lambda.length; ++i) {
       lambda[i] = static_cast<Real>(i);
     }
     WHEN("We use the Get functionality") {
@@ -90,6 +158,65 @@ SCENARIO("IndexableTypes and ManualLambda", "[IndexableTypes]") {
       THEN("We get the correct values") {
         REQUIRE(Zbar == static_cast<Real>(0));
         REQUIRE(lRho == static_cast<Real>(2));
+      }
+    }
+    WHEN("We use the safeGet functionality") {
+      constexpr Real unmodified = -1.0;
+      Real destination = unmodified;
+      WHEN("We request a type that doesn't exist in the manual indexer") {
+        constexpr size_t my_index = 1;
+        const bool modified = safeGet<TableStatus>(lambda, my_index, destination);
+        THEN("The destination WILL be modified since the manual indexer doesn't have the "
+             " `is_type_indexable` data member") {
+          CHECK(modified);
+          REQUIRE(destination == lambda[my_index]);
+        }
+      }
+      WHEN("We define a new manual indexer that has the `is_type_indexable` "
+           "data member") {
+        NewManualLambda_t lambda_new;
+        for (std::size_t i = 0; i < lambda.length; ++i) {
+          lambda_new[i] = lambda[i];
+        }
+        WHEN("We request a type that doesn't exist in the manual indexer") {
+          destination = unmodified;
+          constexpr size_t my_index = 1;
+          const bool modified = safeGet<TableStatus>(lambda_new, my_index, destination);
+          THEN("The destination will NOT be modified") {
+            CHECK(!modified);
+            REQUIRE(destination == unmodified);
+          }
+        }
+      }
+    }
+    WHEN("We use the safeSet functionality") {
+      constexpr Real new_value = -1.0;
+      WHEN("We want to set a value for a type index that doesn't exist") {
+        constexpr size_t my_index = 1;
+        const bool modified = safeSet<TableStatus>(lambda, my_index, new_value);
+        THEN("The lambda value WILL be modified since the manual indexer doesn't have "
+             "the `is_type_indexable` data member") {
+          CHECK(modified);
+          REQUIRE(lambda[my_index] == new_value);
+        }
+      }
+      WHEN("We define a new manual indexer that has the `is_type_indexable` data "
+           "member") {
+        NewManualLambda_t lambda_new;
+        for (std::size_t i = 0; i < lambda.length; ++i) {
+          lambda_new[i] = lambda[i];
+        }
+        WHEN("We request a type that doesn't exist in the manual indexer") {
+          constexpr size_t my_index = 1;
+          const bool modified = safeSet<TableStatus>(lambda_new, my_index, new_value);
+          THEN("The lambda will NOT be modified") {
+            CHECK(!modified);
+            for (std::size_t i = 0; i < lambda.length; ++i) {
+              INFO("i: " << i);
+              CHECK(lambda_new[i] == lambda[i]);
+            }
+          }
+        }
       }
     }
   }
