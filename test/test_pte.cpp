26a1fe644672a849138a12eb9e540fd938ab4dce
Jonah Miller <jonahm@lanl.gov>
2025-12-12 09:44:54 -0500

    test PTE should have a fixed seed for all solvers called, but that varies per trial

--- test/test_pte.cpp
+++ test/test_pte.cpp
@@ -50,7 +50,6 @@ template <template <typename... Types> class Method_t>
 void TestPTE(const std::string name, const std::size_t nscratch_vars,
              std::size_t &nsuccess, std::vector<Real> &host_vals) {
   constexpr Real EPS = 1e-5;
-  Real time;
   nsuccess = 0;
 
   // EOS
@@ -122,6 +121,10 @@ void TestPTE(const std::string name, const std::size_t nscratch_vars,
 #endif
 
   // setup state
+
+  // we want a repeatable random seed every time this function is
+  // called
+  srand(123456789); 
   for (int n = 0; n < NTRIAL; n++) {
     Indexer2D<decltype(rho_hm)> r(n, rho_hm);
     Indexer2D<decltype(vfrac_hm)> vf(n, vfrac_hm);
@@ -218,8 +221,6 @@ void TestPTE(const std::string name, const std::size_t nscratch_vars,
 #endif
 
   Real milliseconds = sum_time.count() / 1e3;
-  time = milliseconds;
-
   std::cout << "Finished " << NTRIAL << " solves in " << milliseconds << " milliseconds"
             << std::endl;
   std::cout << "Solves/ms = " << NTRIAL / milliseconds << std::endl;
