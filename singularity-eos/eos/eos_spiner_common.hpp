c028f7b4427f2c9ff9c79ea4a93e33677c71e02e
Adam Dempsey <adempsey@lanl.gov>
2026-02-26 14:26:31 -0700

    h5_safe_read_string -> h5_safe_read_attr_string

--- singularity-eos/eos/eos_spiner_common.hpp
+++ singularity-eos/eos/eos_spiner_common.hpp
@@ -151,11 +151,12 @@ inline hid_t h5_safe_gopen(hid_t &file, const char *name, hid_t property) {
   return H5Gopen(file, name, property);
 }
 
-inline void h5_safe_read_string(hid_t &file, const char *grp, const char *name,
-                                char *buffer, size_t maxlen) {
+inline void h5_safe_read_attr_string(hid_t &file, const char *grp, const char *name,
+                                     char *buffer, size_t maxlen) {
   if (!buffer || maxlen <= 1) {
-    std::string msg = "Buffer pased into h5_safe_read_string is not large enough for " +
-                      std::string(name);
+    std::string msg =
+        "Buffer pased into h5_safe_read_attr_string is not large enough for " +
+        std::string(name);
     EOS_ERROR(msg);
     return;
   }
--- singularity-eos/eos/eos_spiner_rho_sie.hpp
+++ singularity-eos/eos/eos_spiner_rho_sie.hpp
@@ -470,8 +470,8 @@ herr_t SpinerEOSDependsRhoSieTransformable<TransformerT>::loadDataboxes_(
   if (mfGroup != -1) {
     status += mF_.loadHDF(mfGroup, SP5::Fields::massFrac);
     spiner_common::h5_safe_get_attribute<int>(mfGroup, ".", "numphases", &numphases);
-    spiner_common::h5_safe_read_string(mfGroup, ".", "phase names", phase_names,
-                                       sizeof(phase_names));
+    spiner_common::h5_safe_read_attr_string(mfGroup, ".", "phase names", phase_names,
+                                            sizeof(phase_names));
   } else {
     numphases = 1;
     phase_names[0] = '\0';
