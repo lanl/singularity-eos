9497bf2b22eb90187d9e630d47e6d7f21b1074af
Jonah Miller <jonah.maxwell.miller@gmail.com>
2026-03-20 15:43:45 -0400

    Update singularity-eos/closure/mixed_cell_models.hpp
    Co-authored-by: Jeff Peterson <83598606+jhp-lanl@users.noreply.github.com>

--- singularity-eos/closure/mixed_cell_models.hpp
+++ singularity-eos/closure/mixed_cell_models.hpp
@@ -98,7 +98,7 @@ EnforceMassVolumesSum(const std::size_t nmat, const Real tot_vol, RhoIndexer_t &
   std::size_t imax = 0;
   Real fmax = -1;
   for (std::size_t m = 0; m < nmat; ++m) {
-    PORTABLE_REQUIRE(vfracs[m] > 0, "volume fractions all positive");
+    PORTABLE_REQUIRE(vfracs[m] > 0, "volume fractions must all be strictly positive");
     if (vfracs[m] > fmax) {
       fmax = vfracs[m];
       imax = m;
