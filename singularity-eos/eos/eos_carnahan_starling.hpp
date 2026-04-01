6560a3dc13e773ba12e99175105432cd48cc3807
Jonah Miller <jonahm@lanl.gov>
2026-04-01 09:49:32 -0400

    fix lambdas for C++20

--- singularity-eos/eos/eos_carnahan_starling.hpp
+++ singularity-eos/eos/eos_carnahan_starling.hpp
@@ -113,9 +113,11 @@ class CarnahanStarling : public EosBase<CarnahanStarling> {
 
     // Setup lambda function for finding rho.
     // The equation has been rewritten to avoid division by zero (polynomial equation).
-    auto f = [=](const Real x /* density */) {
+    auto f = [temperature,press,this](const Real x /* density */) {
       const Real eta = _bb * x;
-      const Real term1 = x * (1. + eta + eta * eta - eta * eta * eta);
+      const Real eta2 = eta * eta;
+      const Real eta3 = eta2 * eta;
+      const Real term1 = x * (1. + eta + eta2 - eta3);
       const Real term2 =
           robust::ratio(press * math_utils::pow<3>(1. - eta), _Cv * _gm1 * temperature);
       return term1 - term2;
