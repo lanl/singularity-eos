0fefea68f4bce87a3c29649215aa83324c959022
Adam Dempsey <adempsey@lanl.gov>
2026-07-15 13:07:50 -0600

    Revert "Remove headers that are now in singularity-utils"
    This reverts commit 1665215dee625f2fd20822f3c6864be323ce1dd8.

---
+++ singularity-eos/base/sp5/singularity_eos_sp5.hpp
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// Extra definitions to the SP5 file format required for singularity-eos
+// Author: Jonah Miller (jonahm@lanl.gov)
+// © 2026. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S.  Department of Energy/National
+// Nuclear Security Administration. All rights in the program are
+// reserved by Triad National Security, LLC, and the U.S. Department of
+// Energy/National Nuclear Security Administration. The Government is
+// granted for itself and others acting on its behalf a nonexclusive,
+// paid-up, irrevocable worldwide license in this material to reproduce,
+// prepare derivative works, distribute copies to the public, perform
+// publicly and display publicly, and to permit others to do so.
+//------------------------------------------------------------------------------
+
+#ifndef _SINGULARITY_EOS_BASE_SP5_SINGULARITY_EOS_SP5_HPP_
+#define _SINGULARITY_EOS_BASE_SP5_SINGULARITY_EOS_SP5_HPP_
+
+#include <singularity-utils/sp5/singularity_eos_sp5.hpp>
+
+#endif // _SINGULARITY_EOS_BASE_SP5_SINGULARITY_EOS_SP5_HPP_
